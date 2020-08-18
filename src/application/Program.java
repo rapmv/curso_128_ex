c713b65c54e5956c617140c85db6ad65829f3b83
Raphael Maciel Vilela <raph.mv.cbjr@gmail.com>
2020-08-18 16:03:39 -0300

    Exercicio para estudos

---
+++ src/application/Program.java
@@ -0,0 +1,69 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+import entities.OutsourcedEmployee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the number of employees:");
+		int n = sc.nextInt();
+		
+		List<Employee> list = new ArrayList<>();
+		
+		for (int i=1; i<=n; i++) {
+			System.out.println("Employee #"+i+" data: ");
+			System.out.print("Outsourced (y/n)? ");
+			char ch = sc.next().charAt(0);
+			sc.nextLine();
+			System.out.print("Name: ");
+			String name = sc.nextLine();
+			System.out.print("Hours: ");
+			int hours = sc.nextInt();
+			System.out.print("Value per hour: ");
+			double valuePerHour = sc.nextDouble();
+			
+			if(ch=='y') {
+				System.out.print("Additiona charge: ");
+				double additionalCharge = sc.nextDouble();
+				
+				
+				Employee emp = new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge);
+				list.add(emp);
+				
+				//outro jeito de fazer seria
+				//list.add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+			}
+			else {
+				Employee emp = new Employee(name, hours, valuePerHour);
+				list.add(emp);
+				
+				//outro jeito de fazer seria
+				//list.add(new OutsourcedEmployee(name, hours, valuePerHour));
+			}
+			
+		}
+		System.out.println();
+		System.out.println("PAYMENTS:");
+		
+		for (Employee emp: list) {
+			System.out.println(emp.getName()
+							   +" - $ "
+							   +String.format("%.2f",emp.payment()));
+		}
+		
+		
+		
+		sc.close();
+	}
+
+}
